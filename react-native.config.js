e0cb5f79c492c05794f3e0bfa0914d03a9ee60c7
Vishal Singh <vishal9044singh@gmail.com>
2024-03-28 15:34:00 +0530

    react-native vision camera CameraView not compatible issue resolved

---
+++ react-native.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    project:{
+      android: {
+        unstable_reactLegacyComponentNames: [
+            "CameraView"
+            // list of conponents that needs to be wrapped by the interop layer
+        ]
+      },
+      ios: {
+        unstable_reactLegacyComponentNames: [
+            "CameraView"
+            // list of conponents that needs to be wrapped by the interop layer
+        ]
+      }
+    },
+  };
